1dfb3399c7e368739baa004c1bad708d3ad1b4f8
Phattaradanai Sornsawang <phattaradanai.sor@gmail.com>
2024-09-06 17:51:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,21 +99,21 @@ st.dataframe(df_prediction_proba,
                 'Adelie',
                 format = '%f',
                 width='medium',
-                min_value=0
+                min_value=0,
                 max_value=1
               ),
               'Chinstrap': st.column_config.ProgressColumn(
                 'Chinstrap',
                 format = '%f',
                 width='medium',
-                min_value=0
+                min_value=0,
                 max_value=1
               ),
               'Gentoo': st.column_config.ProgressColumn(
                 'Gentoo',
                 format = '%f',
                 width='medium',
-                min_value=0
+                min_value=0,
                 max_value=1
               ),
             },hide_index=True)
