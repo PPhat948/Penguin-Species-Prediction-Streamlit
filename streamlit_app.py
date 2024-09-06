4d556b4fe2bfce6757619ec7391055fb3142450d
Phattaradanai Sornsawang <phattaradanai.sor@gmail.com>
2024-09-06 17:20:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ def target_encode(val):
 y = y_raw.apply(target_encode)
 
 with st.expander('Data preparation'):
-  st.write('**Encoded input penguin {X}**')
+  st.write('**Encoded X {input penguin}**')
   input_row
   st.write('**Endoded y**')
   y
