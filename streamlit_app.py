a8fe74c1dfb64320ca4fd8cb013f5bfd3024bbab
Phattaradanai Sornsawang <phattaradanai.sor@gmail.com>
2024-09-06 17:50:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,6 +93,35 @@ df_prediction_proba.rename(columns={0:'Adelie',
 
 #Display predicted species
 st.subheader('Predicted Species')
+st.dataframe(df_prediction_proba,
+              column_config={
+              'Adelie': st.column_config.ProgressColumn(
+                'Adelie',
+                format = '%f',
+                width='medium',
+                min_value=0
+                max_value=1
+              ),
+              'Chinstrap': st.column_config.ProgressColumn(
+                'Chinstrap',
+                format = '%f',
+                width='medium',
+                min_value=0
+                max_value=1
+              ),
+              'Gentoo: st.column_config.ProgressColumn(
+                'Gentoo',
+                format = '%f',
+                width='medium',
+                min_value=0
+                max_value=1
+              ),
+            },hide_index=True)
+
+
+
 df_prediction_proba
+
 penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
+#Display predicted species by compare our prediction (0,1,2) with penguin species
 st.success(str(penguins_species[prediction][0]))
